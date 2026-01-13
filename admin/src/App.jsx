bcecb48f631ab24fe11b231f626cc2648175d37c
Krishna Karma <karmakrishna1704@gmail.com>
2026-01-13 19:00:26 +0530

    Fix Sidebar import path for Vercel build

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import SideBar from "./components/SideBar";
+import Sidebar from "./components/Sidebar";
 import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
@@ -35,7 +35,7 @@ const App = () => {
           <Navbar setToken={setToken} />
           <hr />
           <div className="flex w-full">
-            <SideBar />
+            <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
                 <Route path="/add" element={<Add token={token} />} />
